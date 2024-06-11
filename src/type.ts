0d731d6f47397e57a8ac98d69f34698443d7efb3
vidit kushwaha <viditkushwaha530@gmail.com>
2024-06-11 10:10:48 +0530

    updated types of popup

--- src/type.ts
+++ src/type.ts
@@ -1,13 +1,36 @@
-export interface PopupHandle {
-    open: () => void
-    close: () => void
-  }
+import React from "react"
 
-export interface PopupOptions {
-    children: React.ReactNode
-    className?: string
-    closeOnEscape?: boolean | true
-    closeOnOutsideClick?: boolean | true
-    open?: boolean
-    style?:  React.CSSProperties
-    } 
+export interface PopupHandle {
+  open: () => void
+  close: () => void
+}
+
+export interface CloseButtonProps {
+  closePopup: (e: React.MouseEvent<HTMLElement>) => void
+  ariaLabel?: string
+}
+
+export type Id = number | string
+
+
+interface CommonOptions {
+  children: React.ReactNode
+  open?: boolean
+  closeOnClick?: boolean
+  autoClose?: number | false
+  closeOnOutsideClick?: boolean
+  closeOnEscape?: boolean
+  transition?: string | object
+  closeButton?:
+    | boolean
+    | ((props: CloseButtonProps) => React.ReactNode)
+    | React.ReactElement<CloseButtonProps>
+}
+
+export interface PopupProps extends CommonOptions {
+  className?: string
+  style?: React.CSSProperties
+  popupId?: Id
+  updateId?: Id
+  delay?: number
+}
