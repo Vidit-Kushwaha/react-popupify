066e23f3610e9e9a97b0741e915a04ec3d4d1bef
vidit kushwaha <viditkushwaha530@gmail.com>
2024-06-11 10:09:04 +0530

    added sass & sass-loader

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -18,8 +18,8 @@ module.exports = {
         ],
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
