8e75f343079e8c59cc6f9b4df5f4b5f41ac77957
vidit kushwaha <viditkushwaha530@gmail.com>
2024-06-28 15:51:24 +0530

    Testing: Jest setUp and config

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.svg$': 'jest-transformer-svg',
+  },
+}
+export default config
