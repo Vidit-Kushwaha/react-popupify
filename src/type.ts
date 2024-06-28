00ac9e76506662b3908054335dfe303ae1577954
vidit kushwaha <viditkushwaha530@gmail.com>
2024-06-28 15:53:51 +0530

    formated changes

--- src/type.ts
+++ src/type.ts
@@ -178,7 +178,7 @@ export interface ShowPopup extends CommonOptions {
    */
   popupClassName?: string
 
-    /**
+  /**
    * Additional class name for the backdrop.
    */
   backdropClassName?: string
